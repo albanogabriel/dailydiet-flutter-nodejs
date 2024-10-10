74f3b49418c71f941878db3058b2bcdb85f51c4e
Gabriel Albano <albanogabriel33@gmail.com>
2024-10-10 19:48:13 -0300

    feat: declare meals table interface on @types

--- dailydiet-backend/src/@types/knex.d.ts
+++ dailydiet-backend/src/@types/knex.d.ts
@@ -1,4 +1,6 @@
+import { JwtPayload } from "jsonwebtoken"
 import { Knex } from "knex"
+import { CustomJwtPayload } from "../middlewares/check-token-exists"
 
 declare module "knex/types/tables" {
   export interface Tables {
@@ -6,6 +8,16 @@ declare module "knex/types/tables" {
       id: string
       email: string
       password: string
+      created_at: Date
+      updated_at: Date
+    }
+    meals: {
+      id: string
+      user_id: string
+      name: string
+      description: string
+      date_time: Date
+      is_within_diet: boolean
       created_at: Date
       updated_at: Date
     }
