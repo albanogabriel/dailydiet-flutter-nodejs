f4a4d3482702ff2a6bd53593983b3145d0ce418d
Gabriel Albano <albanogabriel33@gmail.com>
2024-10-10 19:49:46 -0300

    feat: routes create meals and get meals created

--- dailydiet-backend/src/routes/meals.ts
+++ dailydiet-backend/src/routes/meals.ts
@@ -1,6 +1,70 @@
 import { FastifyInstance } from "fastify"
 import { knex } from "../database"
+import {
+  checkTokenExists,
+  CustomUserJwtPayload,
+} from "../middlewares/check-token-exists"
+import { z } from "zod"
+import { randomUUID } from "node:crypto"
+import { request } from "node:http"
+import { app } from "../app"
 
 export async function mealsRoutes(app: FastifyInstance) {
-  app.get("/", async () => {})
+  app.post(
+    "/",
+    { preValidation: [checkTokenExists] }, // Middleware para verificar o token
+    async (request, reply) => {
+      const createMealSchema = z.object({
+        name: z.string(),
+        description: z.string().optional(),
+        date_time: z.string(),
+        is_within_diet: z.boolean(),
+      })
+
+      try {
+        const { name, description, date_time, is_within_diet } =
+          createMealSchema.parse(request.body)
+
+        // Use o ID do usuário autenticado a partir do token JWT
+        const userId = (request.user as CustomUserJwtPayload).id
+        console.log(userId)
+
+        // Criação da refeição associada ao usuário
+        await knex("meals").insert({
+          id: randomUUID(),
+          user_id: userId,
+          name,
+          description,
+          date_time: new Date(date_time),
+          is_within_diet,
+          created_at: new Date(),
+          updated_at: new Date(),
+        })
+
+        return reply.status(201).send({ message: "Meal created successfully" })
+      } catch (error) {
+        console.log(error)
+        return reply.status(500).send({ message: "Failed to create meal" })
+      }
+    }
+  )
+
+  app.get(
+    "/",
+    { preValidation: [checkTokenExists] },
+    async (request, reply) => {
+      try {
+        // Use o ID do usuário autenticado a partir do token JWT
+        const userId = (request.user as CustomUserJwtPayload).id
+
+        // Busca as refeições associadas ao usuário
+        const meals = await knex("meals").where("user_id", userId).select("*")
+
+        return reply.status(200).send(meals)
+      } catch (error) {
+        console.log(error)
+        return reply.status(500).send({ message: "Failed to retrieve meals" })
+      }
+    }
+  )
 }
