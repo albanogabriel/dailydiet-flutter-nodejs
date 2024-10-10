9dc5450ded7432abb33946871e59cc0c8d1c173c
Gabriel Albano <albanogabriel33@gmail.com>
2024-10-10 19:49:11 -0300

    feat: allow return userId in request.user on checkTokenExists middlaware

--- dailydiet-backend/src/middlewares/check-token-exists.ts
+++ dailydiet-backend/src/middlewares/check-token-exists.ts
@@ -1,6 +1,11 @@
 import { FastifyReply, FastifyRequest } from "fastify"
 import { app } from "../app"
 
+export interface CustomUserJwtPayload {
+  id: string
+  email: string
+}
+
 export async function checkTokenExists(
   request: FastifyRequest,
   reply: FastifyReply
@@ -13,13 +18,16 @@ export async function checkTokenExists(
 
   // Extraia o token do cabeçalho
   const token = authHeader.split(" ")[1]
+
   if (!token) {
     return reply.status(401).send({ message: "Token missing" })
   }
 
   try {
     // Tenta verificar o token JWT
-    await app.jwt.verify(token)
+    const decoded = (await app.jwt.verify(token)) as CustomUserJwtPayload
+
+    request.user = decoded
   } catch (err) {
     return reply.status(401).send({ message: "Unauthorized" })
   }
