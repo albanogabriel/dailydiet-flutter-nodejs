0260d0244b7cbc57382380100a59eda0a5a27bd2
Gabriel Albano <albanogabriel33@gmail.com>
2024-10-14 14:56:23 -0300

    feat: remove duplicated code

--- dailydiet-backend/src/routes/users.ts
+++ dailydiet-backend/src/routes/users.ts
@@ -40,42 +40,6 @@ export async function usersRoutes(app: FastifyInstance) {
     }
   })
 
-  // Auth user
-  app.post("/login", async (request, reply) => {
-    const loginSchema = z.object({
-      email: z.string().email(),
-      password: z.string(),
-    })
-
-    try {
-      const { email, password } = loginSchema.parse(request.body)
-
-      const user = await knex("users").where({ email }).first()
-
-      if (!user) {
-        return reply.status(400).send({ message: "Invalid credentials" })
-      }
-
-      // Verificar a senha
-      const isPasswordCorrect = await bcrypt.compare(password, user.password)
-
-      if (!isPasswordCorrect) {
-        return reply.status(400).send({ message: "Invalid credentials" })
-      }
-
-      // Gerar token JWT
-      const token = app.jwt.sign({
-        id: user.id,
-        email: user.email,
-      })
-
-      return reply.send({ token })
-    } catch (error) {
-      console.log(error)
-      return reply.status(500).send({ message: "Login failed" })
-    }
-  })
-
   // GET ALL USERS
   app.get(
     "/",
