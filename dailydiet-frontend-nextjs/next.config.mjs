3394310080096f6183ef9f30362593bf28695040
Gabriel Albano <albanogabriel33@gmail.com>
2024-11-02 13:58:33 -0300

    feat: customize native root route "/" for "/home"

--- dailydiet-frontend-nextjs/next.config.mjs
+++ dailydiet-frontend-nextjs/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true, // Set to true for a 308 redirect, or false for a 307 redirect
+      },
+    ];
+  },
+};
 
 export default nextConfig;
