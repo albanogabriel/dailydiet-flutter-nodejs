eac82ed33099eaf487a53cbaa15173092109c4de
albanogabriel <albanogabriel33@gmail.com>
2024-11-28 10:25:21 -0300

    feat(frontend): add dynamic routes on middlaware logic

--- dailydiet-frontend-nextjs/src/middleware.ts
+++ dailydiet-frontend-nextjs/src/middleware.ts
@@ -1,5 +1,3 @@
-// Object.values() -> list a object { dashboad: '/dashboard' , login: '/login:} as itens -> // Object.values(APP_ROUTES.private) = [ "/dashboard", "/home", "/meals" ... ]
-
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
@@ -7,6 +5,7 @@ export const APP_ROUTES = {
   private: {
     home: "/home",
     overview: "/overview",
+    meal: "/meal/:id", // Rota dinâmica com parâmetro `id`
   },
   public: {
     login: "/login",
@@ -19,17 +18,33 @@ export const APP_ROUTES = {
 export default function authMiddleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value
   const isAuthPage = request.nextUrl.pathname === APP_ROUTES.public.login
-  const isProtectedRoute = Object.values(APP_ROUTES.private).includes(
+
+  // Valida rotas privadas estáticas
+  const isStaticProtectedRoute = Object.values(APP_ROUTES.private).includes(
     request.nextUrl.pathname,
   )
 
-  // Redirect unauthenticated users from protected routes to the login page
+  // Valida rotas privadas dinâmicas
+  const isDynamicProtectedRoute = Object.values(APP_ROUTES.private).some(
+    (route) => {
+      if (route.includes("/:")) {
+        // Remove o parâmetro dinâmico e verifica a base da rota
+        const baseRoute = route.split("/:")[0]
+        return request.nextUrl.pathname.startsWith(baseRoute)
+      }
+      return false
+    },
+  )
+
+  const isProtectedRoute = isStaticProtectedRoute || isDynamicProtectedRoute
+
+  // Redireciona usuários não autenticados para a página de login se estiverem em uma rota protegida
   if (isProtectedRoute && !token) {
     const loginUrl = new URL(APP_ROUTES.public.login, request.url)
     return NextResponse.redirect(loginUrl)
   }
 
-  // Redirect authenticated users away from the login page to the home page
+  // Redireciona usuários autenticados para a home se estiverem na página de login
   if (isAuthPage && token) {
     const homeUrl = new URL(APP_ROUTES.private.home, request.url)
     return NextResponse.redirect(homeUrl)
@@ -39,7 +54,17 @@ export default function authMiddleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [...Object.values(APP_ROUTES.private), APP_ROUTES.public.login],
+  matcher: [
+    ...Object.values(APP_ROUTES.public), // Rotas públicas
+    ...Object.values(APP_ROUTES.private).map((route) => {
+      // Verifica se a rota é dinâmica (contém :id)
+      if (route.includes("/:")) {
+        const baseRoute = route.split("/:")[0]
+        return `${baseRoute}/:path*` // Ajusta a rota dinâmica para capturar sub-rotas
+      }
+      return route
+    }),
+  ],
 }
 
 // orignal middleware.ts
