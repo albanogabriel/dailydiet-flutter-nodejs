d1d9ab1c81c0bf4dbae6ee82c7f0d6cdbc3bf023
albanogabriel <albanogabriel33@gmail.com>
2024-11-28 10:31:15 -0300

    fix(frontend): throwback arrow bar removed from public layout

--- dailydiet-frontend-nextjs/src/app/(public)/public-layout.tsx
+++ dailydiet-frontend-nextjs/src/app/(public)/public-layout.tsx
@@ -1,19 +1,6 @@
 // app/components/PublicLayout.tsx
 import { ReactNode } from "react"
-import { ToggleThemeButton } from "../_components/toggle-theme-button"
-import { ArrowLeft } from "lucide-react"
-import { useRouter } from "next/navigation"
 
 export default function PublicLayout({ children }: { children: ReactNode }) {
-  const router = useRouter()
-
-  return (
-    <main>
-      <div className="flex w-full items-center justify-between p-4 lg:justify-end">
-        <ArrowLeft onClick={() => router.back()} className="lg:hidden" />
-        <ToggleThemeButton />
-      </div>
-      {children}
-    </main>
-  )
+  return <main className="flex-1">{children}</main>
 }
